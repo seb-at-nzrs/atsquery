964f1f2415a448b5111df218d32dd59d61c18f4e
Sebastian Castro <sebastian@nzrs.net.nz>
2016-03-02 17:51:10 +1300

    Changes to make it work while hidding the secret tokens

--- readme.md
+++ readme.md
@@ -7,19 +7,31 @@ using your Access Key ID and Secret Access Key.
 Requirements:  
   * Python v2.7.3  
   * [Requests: HTTP for Humans &rarr;](http://python-requests.org)  
-  * [lxml: XML and HTML with Python &rarr;](http://lxml.de)  
+  * [xml.etree.ElementTree: XML parser for Python &rarr;](http://effbot.org/zone/element-index.htm)
 
 Steps:  
   * Sign up for an [Amazon Web Services &rarr;](http://aws.amazon.com)  
   * Get your `Access Key ID` and `Secret Access Key`  
   * Sign up for [Alexa Top Sites &rarr;](http://aws.amazon.com/alexatopsites)  
   * Install all requirements using `pip`  
-  * Run `topsites.py`  
+  * Run `topsites.py --auth auth.yaml --country XX`  
 
 Installation and Usage:
 
     $ pip install -r requirements.txt
-    $ python topsites.py ACCESS_KEY_ID SECRET_ACCESS_KEY [COUNTRY_CODE]
+    $ python topsites.py --auth auth.yaml --country XX
+
+Authorization File:
+The format for the authorization file is:
+
+```
+access_key_id: ACCESS_KEY_ID
+secret_access_key: SECRET_ACCESS_KEY
+```
+
+The main benefit is to avoid having your access token visible in the command 
+line, which can be retrieved by a super user with `ps` or using the /proc 
+filesystem
 
 If you are getting "Not Authorized" messages, 
 you probably have one of the following problems:
--- topsites.py
+++ topsites.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/ python
 
 """query requests to alexa top sites web service
-usage: python topsites.py ACCESS_KEY_ID SECRET_ACCESS_KEY [COUNTRY_CODE]
+usage: python topsites.py --auth AUTH_FILE [--country XX] [--country XX]
 """
 
 __author__ = "Vaibhav Bajpai (contact@vaibhavbajpai.com)"
@@ -9,97 +9,111 @@ __date__ = "$Date: 2012/12/16 15:45:39 $"
 __copyright__ = "Copyright (c) 2012 Vaibhav Bajpai"
 __license__ = "Python"
 
-import sys
 import requests
 import datetime
 import hmac
 import hashlib
 import base64
-import urllib
 import collections
-import lxml.etree
+import xml.etree.ElementTree as ET
+import yaml
+import argparse
 
-host = 'ats.amazonaws.com'
-action = 'TopSites'
+"""Base constant setup"""
+HOST = 'ats.amazonaws.com'
+ACTION = 'TopSites'
+TIMESTAMP = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000Z")
+RESPONSE_GROUP = "Country"
+START = 1
+COUNT = 100
+SIGNATURE_VERSION = 2
+SIGNATURE_METHOD = 'HmacSHA256'
+
 access_key_id = None
-timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000Z")
-response_group = "Country"
-start = 1
-count = 100
 country_code = ''
-signature_version = 2
-signature_method = 'HmacSHA1'
+
 
 def http_get(access_key_id, secret_access_key, country_code='', signature=''):
-  """sends a HTTP GET to alexa top sites web service using requests;
-     parses the XML response using lxml; filters the response XML for domain
+    """sends a HTTP GET to alexa top sites web service using requests;
+     parses the XML response using xml; filters the response XML for domain
      names and returns the list of domain entries"""
 
-  query = {
-      "Action"             :     action
-    , "AWSAccessKeyId"     :     access_key_id
-    , "Timestamp"          :     timestamp
-    , "ResponseGroup"      :     response_group
-    , "Start"              :     start
-    , "Count"              :     count
-    , "CountryCode"        :     country_code
-    , "SignatureVersion"   :     signature_version
-    , "SignatureMethod"    :     signature_method
-  }
+    query = {
+        "Action"           : ACTION,
+        "AWSAccessKeyId"   : access_key_id,
+        "Timestamp"        : TIMESTAMP,
+        "ResponseGroup"    : RESPONSE_GROUP,
+        "Start"            : START,
+        "Count"            : COUNT,
+        "CountryCode"      : country_code,
+        "SignatureVersion" : SIGNATURE_VERSION,
+        "SignatureMethod"  : SIGNATURE_METHOD
+    }
 
-  query = collections.OrderedDict(sorted(query.items()))
-  req = requests.Request (
-                            method='GET'
-                          , url='http://%s'%(host)
-                          , params=query
-                         )
-  try: prep = req.prepare()
-  except Exception as e: print e
-
-  string_to_sign = '\n'.join([prep.method, host, '/', prep.path_url[2:]])
-  signature = hmac.new (
-                          key=secret_access_key
-                        , msg=bytes(string_to_sign)
-                        , digestmod=hashlib.sha1
-                       ).digest()
-  signature = base64.b64encode(signature)
-  prep.url = '%s&Signature=%s'%(prep.url, signature)
-  s = requests.Session()
-  try: res = s.send(prep)
-  except Exception as e: print e
-  else:
+    query = collections.OrderedDict(sorted(query.items()))
+    req = requests.Request(
+                            method='GET',
+                            url='http://%s' % HOST,
+                            params=query
+                          )
     try:
-      if res.status_code is not requests.codes.ok: res.raise_for_status()
+        prep = req.prepare()
     except Exception as e:
-      print e
-      print res.text
+        print e
 
-  xml = res.text
-  try: tree = lxml.etree.fromstring(xml)
-  except Exception as e: print e
-  NSMAP = {'aws' : 'http://ats.amazonaws.com/doc/2005-11-21'}
-  try: entries = tree.xpath('//aws:DataUrl', namespaces = NSMAP)
-  except Exception as e: print e
-  entries = [entry.text for entry in entries]
-  return entries
+    string_to_sign = '\n'.join([prep.method, HOST, '/', prep.path_url[2:]])
+    print(string_to_sign)
+    signature = hmac.new(
+        key=secret_access_key,
+        msg=bytes(string_to_sign),
+        digestmod=hashlib.sha256).digest()
+    signature = base64.b64encode(signature)
+    prep.url = '%s&Signature=%s'%(prep.url, signature)
 
-def main(args):
-  """parses command line arguments and calls http_get(...); 
-     retrieves the list of entries and prints them out"""
-  access_key_id = args[0]
-  secret_access_key = args[1]
-  country_code = len(args) > 2 and args[2] or ''
-  try:
-    entries = http_get(access_key_id, secret_access_key, country_code)
-    for entry in entries: print entry
-  except TypeError as e: print e
+    s = requests.Session()
+    try:
+        res = s.send(prep)
+    except Exception as e:
+        print e
+    else:
+        try:
+            if res.status_code is not requests.codes.ok:
+                res.raise_for_status()
+        except Exception as e:
+            print e
+            print res.text
+
+    xml = res.text
+    entries = []
+    NSMAP = {'aws': 'http://ats.amazonaws.com/doc/2005-11-21'}
+    try:
+        tree = ET.fromstring(xml)
+        xml_elems = tree.findall('.//aws:DataUrl', NSMAP)
+        entries = [entry.text for entry in xml_elems]
+    except Exception as e:
+        print e
+
+    return entries
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Fetches Top 100 sites per country "
+                                     "from Alexa Top Sites")
+    parser.add_argument('--auth', type=str, required=False, default='auth.yaml',
+                        help="YAML file with the access credentials")
+    parser.add_argument('--country', type=str, nargs='+', required=True,
+                        help="Country to fetch Top 100 sites from")
+    args = parser.parse_args()
 
-  def usage():
-    """echoes the usage and exits"""
-    print __doc__
-    sys.exit(2)
+    """Read the auth credentials"""
+    with open(args.auth, 'r') as f:
+        auth = yaml.load(f)
 
-  if len(sys.argv) < 3: usage()
-  else: main(sys.argv[1:])
+    for country in args.country:
+        try:
+            print("Country: %s" % country)
+            for entry in http_get(auth['access_key_id'],
+                                  auth['secret_access_key'], country):
+                print entry
+        except TypeError as e:
+            print e
